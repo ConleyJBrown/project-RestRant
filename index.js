88b0b35c3a47d0083454c96f094257e3f1e62d9e
Conley Brown <conleybrown@Conleys-MacBook-Pro.local>
2022-05-24 13:44:18 -0400

    Adding styles to REST-Rant

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+
+//requirements
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const placesRoutes = require('./controllers/places')
 
 
-
-
+//Express settings
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
-app.use('/places', require('./controllers/places'))
+app.use('/places', placesRoutes)
+app.use(express.static('public'))
 
+//routes
 app.get('/', (req, res) => {
     res.render('home')
 })
